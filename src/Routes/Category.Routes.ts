cb0112cdaaff83b9fdea3619d51457a133689d92
jefferson <jeffesilva546@gmail.com>
2024-05-27 13:17:46 -0300

    Your commit message

--- src/Routes/Category.Routes.ts
+++ src/Routes/Category.Routes.ts
@@ -5,18 +5,28 @@ import { CategoryRepository } from '../repositories/Category.repositories';
 const CategoryRoutes = Router();
 const CategoriesRespositories = new CategoryRepository();
 
-CategoryRoutes.post('/create',(request,response)=>{
+CategoryRoutes.post('/create',async (request,response)=>{
     const { name , description} = request.body;
+    try{
+       const result = await CategoriesRespositories.findByname(name);
+       if(result){
+        return response.status(400).json({Error:'usuario já existe'})
+       }
+    }catch(err){
+        console.log(err);
+    }
+    
     CategoriesRespositories.create({ name, description});
     return response.send().status(201);
 })
 CategoryRoutes.get('/category', async (request: Request, response: Response) => {
-  try {
-      const result = await CategoriesRespositories.list();
-      response.json(result);
-  } catch (error) {
-      response.status(500).json({ error: 'Erro ao listar categorias' });
-  }
+    try {
+        const result = await CategoriesRespositories.list();
+        return response.json(result);
+    } catch (error) {
+        console.error('Erro ao listar categorias:', error);
+        return response.status(500).json({ error: 'Erro ao listar categorias' });
+    }
 });
 
 export { CategoryRoutes };
--- src/repositories/Category.repositories.ts
+++ src/repositories/Category.repositories.ts
@@ -1,41 +1,60 @@
-
-
 import { connection } from "../connection/connection";
 import { CreateCategoria } from "../model/Category";
 
-interface ICreateCategoryDTO{
-    name:string;
-    description:string;
-    
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+
 }
-class CategoryRepository{
-  create({name,description}:ICreateCategoryDTO){
-  const Category = new CreateCategoria(name,description);
-  const sql = "INSERT INTO Category (id,name , description , CreateAT) VALUES(?,?,?,?)";
-    Object.assign(Category,{
-      name,
-      description,
-      CreateAT : new Date()
-    })
-   connection.query(sql,[Category.getId(),Category.getName,Category.getDescription,Category.getCreateAT],(err)=>{
-      if(err){
+class CategoryRepository {
+  create({ name, description }: ICreateCategoryDTO): void {
+
+    const Category = new CreateCategoria(name, description);
+    const sql = "INSERT INTO Category (id,name , description , CreateAT) VALUES(?,?,?,?)";
+    connection.query(sql, [Category.getId(), Category.getName(), Category.getDescription(), Category.getCreateAT()], (err) => {
+      if (err) {
         console.log(err);
-      }else{
+      } else {
         console.log("Categoria criada com sucesso");
       }
     })
- }
- list(){
+    
+  }
+  async list() {
     const sql = "SELECT * FROM Category";
-    connection.query(sql,(err,result)=>{
-      if(err){
-        console.log(err);
-      }else{
-        console.log(result)
-        return  result;
-      }
-    })
-   
- }
+    try {
+      const result = await new Promise((resolve, reject) => {
+        connection.query(sql, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+      console.log(result);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async findByname(name: string) {
+    const sql = "SELECT name FROM Category WHERE name = ?";
+    try {
+      const result = await new Promise((resolve, reject) => {
+        connection.query(sql,[name],(err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+      console.log(result);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
-export {CategoryRepository};
+export { CategoryRepository };
